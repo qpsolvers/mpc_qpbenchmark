9b894c1af0a9a445c296d44c0480c3fd286c96d5
Stéphane Caron <stephane.caron@normalesup.org>
2023-12-13 18:58:17 +0100

    Generate QUADMPC problems
    See https://github.com/qpsolvers/qpbenchmark/issues/93

---
+++ generate/quadcmpc.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2023 Inria
+# SPDX-License-Identifier: Apache-2.0
+
+from os import path
+
+import numpy as np
+from qpsolvers import Problem
+
+from quadruped_qp_problems.centroidalMPC import CentroidalQuadruped
+
+DATA_DIR = path.realpath(
+    path.join(path.dirname(path.realpath(__file__)), "../data")
+)
+
+
+def generate_problem(name: str, gait: np.ndarray, v_ref: np.ndarray):
+    centroidal_problem = CentroidalQuadruped(gait, v_ref)
+    problem = Problem(
+        P=centroidal_problem.get_P(),
+        q=centroidal_problem.get_q(),
+        G=centroidal_problem.get_G(),
+        h=centroidal_problem.get_h(),
+        A=centroidal_problem.get_A(),
+        b=centroidal_problem.get_b(),
+        lb=centroidal_problem.get_lb(),
+        ub=centroidal_problem.get_ub(),
+    )
+    save_path = f"{DATA_DIR}/{name}.npz"
+    print(f"Writing {name} problem to {save_path}...")
+    problem.save(save_path)
+
+
+if __name__ == "__main__":
+    gait = np.array(
+        [
+            [8, 1, 0, 0, 1],
+            [8, 0, 1, 1, 0],
+            [8, 1, 0, 0, 1],
+            [8, 0, 1, 1, 0],
+        ]
+    )
+    v_ref = np.array([0.1, 0.0, 0.0]).reshape((-1, 1))
+    generate_problem("QUADCMPC1", gait, v_ref)
+
+    gait = np.array(
+        [
+            [12, 1, 0, 0, 1],
+            [12, 0, 1, 1, 0],
+        ]
+    )
+    v_ref = np.array([0.1, 0.0, 0.0]).reshape((-1, 1))
+    generate_problem("QUADCMPC2", gait, v_ref)
+
+    gait = np.array([[16, 1, 1, 1, 1]])
+    vref = np.zeros((3, 1))
+    generate_problem("QUADCMPC3", gait, v_ref)
+
+    gait = np.array([[8, 1, 0, 0, 1], [8, 0, 1, 1, 0]])
+    vref = np.zeros((3, 1))
+    generate_problem("QUADCMPC4", gait, v_ref)
